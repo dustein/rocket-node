e54d282da9ecf04b7d44b9c6ca5d65095222b621
dustein <steindu@gmail.com>
2021-12-03 22:23:01 -0300

    atualiza

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
 const express = require('express')
+const { v4: uuidv4 } = require('uuid')
 const app = express()
-app.listen(3333)
+app.use(express.json())
 
-app.get("/",
-    (req, res) => {
-        res.send("oi funciona")
+const customers = []
+
+app.get("/account", (req, res) => {
+    res.json({status: "ok"})
+})
+
+app.post("/account", (req, res) => {
+    const { cpf, name } = req.body
+    const id = uuidv4()
+
+    customers.push({
+        id,
+        cpf,
+        name,
+        statement: []
     })
+    res.status(201).json(customers)
+})
+
+app.put("account/:id", (req, res) => {
+    res.json(ok)
+})
+    
+app.patch("account/:id", (req, res) => {
+    res.json(ok)
+})
+
+app.delete("account/:id", (req, res) => {
+    res.json(Ok)
+})
+
+
+app.listen(3333)
